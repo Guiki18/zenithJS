3c43e1a293e24bc9424b48bbcdbfdc54b8a8d423
Guiki18 <foxzillapt17@gmail.com>
2022-06-04 14:56:16 +0000

    last day update

--- web/keepAlive.js
+++ web/keepAlive.js
@@ -3,7 +3,7 @@ const colors = require('colors/safe');
 module.exports = async function() {
 	const express = require('express');
 	const app = express();
-	const port = 3000;
+	const port = 8080;
 	app.get('/', (req, res) => res.send('Hello World!'));
 	app.listen(port, () => console.log(colors.brightYellow(`App is listening at http://localhost:${port}`)));
 };
